eba56b12517314cacf1d30a0d024efec3e7bafec
MuKyoung_Desktop_0 <hsib1212@naver.com>
2025-12-24 17:22:32 +0900

    feat: [박무경] 자바스크립트 데이터 타입 및 변수 [PMIF-x]

--- parkmukyoung/javascript/javascript-app/src/App.tsx
+++ parkmukyoung/javascript/javascript-app/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { CalculationExample } from '../math_operation/CalcuationExample'
+import { VariableAndDataTypes } from './data_type/VariableAndDataTypes'
+import { CalculationExample } from './math_operation/CalcuationExample'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
             지저분해지는 상황을 방지할 수 있습니다. */}
         {/* 관심사의 분리를 달성할 수 있음 */}
         <CalculationExample/>
+        <VariableAndDataTypes/>
       </div>
     </>
   )
